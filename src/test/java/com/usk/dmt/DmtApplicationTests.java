b68fe07b12ed366f181c9ba9824ed835dab4db38
gangasaiuskcorp <gangasai.uskcorp@gmail.com>
2022-01-06 18:22:19 +0530

    First commit

---
+++ src/test/java/com/usk/dmt/DmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usk.dmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
