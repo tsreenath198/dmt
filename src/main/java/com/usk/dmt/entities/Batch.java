1748ce7207558f9b4fb9f5c2d05045b67e292846
gangasaiuskcorp <gangasai.uskcorp@gmail.com>
2022-02-17 17:22:58 +0530

    Added entities for batch and trainer

---
+++ src/main/java/com/usk/dmt/entities/Batch.java
@@ -0,0 +1,65 @@
+package com.usk.dmt.entities;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+
+@Entity
+@Table(name = "batch")
+@Data
+public class Batch implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "active_flag")
+    private int activeFlag;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "created_date")
+    private Date createdDate;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "duration")
+    private int duration;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "end_date")
+    private Date endDate;
+
+    @Column(name = "paid_status")
+    private String paidStatus;
+
+    @Column(name = "received_status")
+    private String receivedStatus;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "start_date")
+    private Date startDate;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "time")
+    private String time;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "updated_date")
+    private Date updatedDate;
+
+    //bi-directional many-to-one association to Technology
+    @ManyToOne
+    private Technology technology;
+
+    //bi-directional many-to-one association to Trainer
+    @ManyToOne
+    private Trainer trainer;
+
+
+}
