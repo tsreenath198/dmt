26cfd6e7ccf3ff56069f7d1ca0ead662c491991c
gangasaiuskcorp <gangasai.uskcorp@gmail.com>
2022-02-25 17:16:24 +0530

    added uri constants

--- src/main/java/com/usk/dmt/constants/URIConstants.java
+++ src/main/java/com/usk/dmt/constants/URIConstants.java
@@ -9,4 +9,18 @@ public interface URIConstants {
     String TECHNOLOGY = "/technology";
     String TRAINER = "/trainer";
     String BATCH = "/batch";
+    String CLIENT = "/client";
+    String CONTACT = "/contact";
+    String COURSE = "/course";
+    String EMPLOYEE = "/employee";
+    String INTERVIEW = "/interview";
+    String INVOICE = "/invoice";
+    String PIPELINE = "/pipeline";
+    String QUESTION = "/question";
+    String RESUME = "/resume";
+    String SALARY = "/salary";
+    String SUPPORT = "/support";
+    String TODO = "/todo";
+    String TRAINEE = "/trainee";
+    String USER = "/user";
 }
